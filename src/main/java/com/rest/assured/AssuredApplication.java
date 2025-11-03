44afc5f1073875a24aef128b1a0dad8b1782c274
Warren Goldman <57532603+warrengoldman@users.noreply.github.com>
2025-11-03 10:16:25 -0600

    Initial commit

---
+++ src/main/java/com/rest/assured/AssuredApplication.java
@@ -0,0 +1,13 @@
+package com.rest.assured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssuredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssuredApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rest/assured/AssuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.assured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssuredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
